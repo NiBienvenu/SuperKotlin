a203831a868546674695d693ed011e13a5f01a40
“NiBienvenu” <nicibienvenu@gmail.com>
2024-08-04 08:56:05 +0200

    Autre texte ..

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,37 +1,33 @@
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import component.Input
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import superkotlin.composeapp.generated.resources.Res
-import superkotlin.composeapp.generated.resources.compose_multiplatform
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
         var showContent by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                    Input()
-                }
-            }
+        val state  by remember{ mutableStateOf("") }
+        Row {
+            TextField( value = state, onValueChange = {}, keyboardActions = KeyboardActions.Default)
+            Text("Hello ..", fontSize = 20.sp, fontWeight = FontWeight.Bold)
+
+
         }
     }
 }
--- composeApp/src/commonMain/kotlin/component/InputCustomer.kt
+++ composeApp/src/commonMain/kotlin/component/InputCustomer.kt
@@ -15,7 +15,7 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 fun Input() {
     val value = remember { mutableStateOf("") }
     Column {
-        Text("Autre texte")
+        Text("Autre texte ...")
 
     }
 }

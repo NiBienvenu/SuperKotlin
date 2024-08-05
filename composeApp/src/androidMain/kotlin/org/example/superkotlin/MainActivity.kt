a3e2e817719b89e83bbbb2c2525009d9224e0726
“NiBienvenu” <nicibienvenu@gmail.com>
2024-08-05 15:46:17 +0200

    hello Word

--- composeApp/src/androidMain/kotlin/org/example/superkotlin/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/org/example/superkotlin/MainActivity.kt
@@ -8,18 +8,30 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.Colors
 import androidx.compose.material.Icon
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material.icons.rounded.Check
+import androidx.compose.material.icons.rounded.Person
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import java.nio.file.WatchEvent.Modifier
 
 class MainActivity : ComponentActivity() {
@@ -35,14 +47,25 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true, showSystemUi = true, device = "spec:id=reference_phone,shape=Normal,width=411,height=891,unit=dp,dpi=420")
 @Composable
 fun AppAndroidPreview() {
-    Column {
+    var text by remember {
+        mutableStateOf("")
+    }
+    Surface() {
+        Column(
+//            modifier = Modifier.fillMaxWidth()
+//                .padding(16.sp)
+        ) {
 
-        Text(text = "hello Word")
-        Text(text = "hello Word")
-        Text(text = "hello Word")
-        Row {
-            Icon(imageVector = Icons.Default.Add, contentDescription = null )
-            Icon(imageVector = Icons.Default.Person, contentDescription = null)
+            Text(text = "hello Word Bienvenu")
+            Text(text = "hello Word")
+            Text(text = "hello Word")
+            Row {
+                Icon(imageVector = Icons.Outlined.Person, contentDescription = null )
+                Icon(imageVector = Icons.Default.Person, contentDescription = null)
+            }
+            Text(text = text)
+            OutlinedTextField(value = text, onValueChange = {text=it} , )
+
         }
     }
 }
